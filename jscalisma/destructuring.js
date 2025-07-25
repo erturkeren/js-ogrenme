43ee93dd075ea9acd7a4b0c501c717b6d8a4136c
EREN ERTÜRK <erturkeren@outlook.com>
2025-07-25 15:33:31 +0300

    Create destructuring.js

---
+++ jscalisma/destructuring.js
@@ -0,0 +1,54 @@
+/// DESTRUCTURING
+
+// [a, b] = [10, 20];
+
+// // console.log(a);
+// // console.log(b);
+
+// [a, b, ...rest] = [10, 20, 30, 40, 50, 60];
+
+// // console.log(a);
+// // console.log(b);
+// // console.log(rest);
+
+// ({ a, b } = { a: 10, b: 20 });
+// // console.log(a);
+// // console.log(b);
+
+// ({ a, b, ...rest } = { a: 10, b: 20, c: 30, d: 40 });
+
+// // console.log(a);
+// // console.log(b);
+// // console.log(rest);
+
+// /// ARRAY DESTRUCTURING
+// // let arrConfig = ["localhost", "8080", "900"];
+
+// // let [server, port, timeout] = arrConfig;
+// // console.log(server, port, timeout);
+
+// ///Object Destructuring
+
+// // let objConfig = {
+// //   server: "localhost",
+// //   port: "8080",
+// //   timeout: 700,
+// // };
+
+// // let server = objConfig.server;
+// // let port = objConfig.port;
+// // let timeout = objConfig.timeout;
+
+// // let { server, port, timeout } = objConfig;
+
+// // console.log(server, port, timeout);
+
+// // let { timeout: t } = objConfig;
+// // console.log(t);
+
+// // let { server, port, timeout = 800 } = objConfig;
+// // console.log(server, port, timeout);
+
+// // let days = ["monday", "tuesday", "wednesday", "thursday", "friday"];
+// // let [, , wed, , fri] = days;
+// // console.log(wed, fri);
